f5faa9cfce98747d3a5dec181b6bf59ea328369d
Alfred Hutomo <s3790172@student.rmit.edu.au>
2021-11-10 16:13:21 +1100

    frontend and backend structure added

---
+++ relab-strapi-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f003adb9edb4cf9543622e7024a27f81'),
+    },
+  },
+});
---
+++ relab-strapi-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca4c2c0f-2d3d-408e-a5a1-604b7be1fce1'
+};
