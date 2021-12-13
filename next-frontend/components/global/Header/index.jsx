9c54fb63babd37f93e6657059b171ab9b61438c2
Alfred Hutomo <44387641+AlfredHutomo@users.noreply.github.com>
2021-12-13 22:13:25 +0700

    fixed nextjs severe crash in server and added SectionWrapper padding prop

--- next-frontend/components/global/Header/index.jsx
+++ next-frontend/components/global/Header/index.jsx
@@ -4,6 +4,7 @@ import LogoText from '../../../public/Logo_With_Text.svg';
 
 import { MdMenu } from 'react-icons/md';
 
+import SectionWrapper from '../SectionWrapper';
 import Button from '../Button';
 import { useState } from 'react';
 
@@ -26,29 +27,31 @@ const Header = () => {
             <div className={styles.banner}>
                 <p className={styles.text}>Celebrating 10 years in the game!</p>
             </div>
-            <header className={styles.header}>
-                <div className={styles.container}>
-                    <div className={styles.logo}>
-                        <Image src={LogoText} height={54} width={202} />
-                        {mobileMenu ? (
-                            <></>
-                        ) : (
-                            <MdMenu onClick={toggleMobileMenu} />
-                        )}
+            <SectionWrapper padding={false}>
+                <header className={styles.header}>
+                    <div className={styles.container}>
+                        <div className={styles.logo}>
+                            <Image src={LogoText} height={54} width={202} />
+                            {mobileMenu ? (
+                                <></>
+                            ) : (
+                                <MdMenu onClick={toggleMobileMenu} />
+                            )}
+                        </div>
+                        <nav className={styles.navigation}>
+                            {links.map((link, i) => (
+                                <div key={i} className={styles.links}>
+                                    <h6 className={styles.h6}>{link}</h6>
+                                </div>
+                            ))}
+                        </nav>
+                        <div className={styles.buttons}>
+                            <Button variant={'3'}>Member portal</Button>
+                            <Button variant={'1'}>Register</Button>
+                        </div>
                     </div>
-                    <nav className={styles.navigation}>
-                        {links.map((link, i) => (
-                            <div key={i} className={styles.links}>
-                                <h6 className={styles.h6}>{link}</h6>
-                            </div>
-                        ))}
-                    </nav>
-                    <div className={styles.buttons}>
-                        <Button variant={'3'}>Member portal</Button>
-                        <Button variant={'1'}>Register</Button>
-                    </div>
-                </div>
-            </header>
+                </header>
+            </SectionWrapper>
         </>
     );
 };
--- next-frontend/components/global/SectionWrapper/index.jsx
+++ next-frontend/components/global/SectionWrapper/index.jsx
@@ -1,26 +1,26 @@
 import styles from './sectionWrapper.module.scss';
+import PropTypes from 'prop-types';
 
 const SectionWrapper = (props) => {
-
-    const {
-        bgcolor
-    } = props;
-
-
-    const wrapperStyle = {
-        //padding: '0 20px',
-    };
-
+    const { bgcolor, padding } = props;
 
     return (
-        <div className={styles['section-wrapper']} style={{ backgroundColor: props.bgcolor }}>
-            <div className={styles['section-content']}>
-                {props.children}
-            </div>
+        <div
+            className={padding ? styles['section-wrapper'] : ''}
+            style={{ backgroundColor: props.bgcolor }}
+        >
+            <div className={styles['section-content']}>{props.children}</div>
         </div>
     );
 };
 
+SectionWrapper.propTypes = {
+    bgcolor: PropTypes.string,
+    padding: PropTypes.bool,
+};
+
+SectionWrapper.defaultProps = {
+    padding: true,
+};
 
 export default SectionWrapper;
-
