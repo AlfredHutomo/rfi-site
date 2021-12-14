a132d2cebad1a5c5e119bfb39734a936c9407158
Alfred Hutomo <alfredhutomo@gmail.com>
2021-12-14 17:06:53 +0700

    worked on data trimming in the custom controller for programs for tags

--- backend/src/api/home-page/controllers/home-page.js
+++ backend/src/api/home-page/controllers/home-page.js
@@ -10,11 +10,19 @@ module.exports = createCoreController(
   "api::home-page.home-page",
   ({ strapi }) => ({
     async find(ctx) {
-      const populateList = ["Hero.hero_card"];
+      // make an array of parts that needed to be populated
+      const populateList = [
+        "hero.hero_card",
+        "programs.tags",
+        "block_tiles.content",
+      ];
 
+      // push the items from the ctx.query.populate to the array
+      // and joining it together with a comma.
       populateList.push(ctx.query.populate);
       ctx.query.populate = populateList.join(",");
 
+      // trigger the find method from the coreController
       const respond = await super.find(ctx);
 
       const { data } = respond;
