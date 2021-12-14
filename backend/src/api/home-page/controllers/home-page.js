8163003d73ca225d94179d4496c95b72f5bd63db
Alfred Hutomo <alfredhutomo@gmail.com>
2021-12-14 14:04:14 +0700

    pinned volta node version and added logic for custom controller for home page single types

--- backend/src/api/home-page/controllers/home-page.js
+++ backend/src/api/home-page/controllers/home-page.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  *  home-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::home-page.home-page');
+module.exports = createCoreController(
+  "api::home-page.home-page",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const populateList = ["Hero.hero_card"];
+
+      populateList.push(ctx.query.populate);
+      ctx.query.populate = populateList.join(",");
+
+      const respond = await super.find(ctx);
+
+      const { data } = respond;
+
+      return data;
+    },
+  })
+);
