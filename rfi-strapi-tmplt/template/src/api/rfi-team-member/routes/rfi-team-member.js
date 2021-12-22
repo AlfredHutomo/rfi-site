ef27c2945a33919bdb89fadbf17fff4212e5a079
Alfred Hutomo <alfred@relab.com.au>
2021-12-22 22:47:18 +0700

    updated strapi to 4.0.2 and create strapi template based on the current backend

---
+++ rfi-strapi-tmplt/template/src/api/rfi-team-member/routes/rfi-team-member.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rfi-team-member router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rfi-team-member.rfi-team-member');
