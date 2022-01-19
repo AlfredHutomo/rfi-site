50a97bfdeb044cad1f7d96f12860ad31b60234f8
Sam.S <sam@relab.com.au>
2022-01-20 10:39:24 +1100

    add stats block option for dark bg

--- next-frontend/components/StatisticsBlock/index.jsx
+++ next-frontend/components/StatisticsBlock/index.jsx
@@ -6,14 +6,23 @@ import styles from './StatisticsBlock.module.scss';
 import Button from '../global/Button';
 
 const StatisticsBlock = (props) => {
-    const { heading, statsData } = props;
+    const { heading, statsData, isDarkBackground } = props;
+    const altStyle = (isDarkBackground != null) ? styles['darkBg'] : '';
 
     return (
-        <div className={styles['statistics-block-wrapper']}>
-            <h2 className={'h2 ' + styles['statistics-block-heading']}>{props.heading}</h2>
-            <div className={styles['statistics-block-description']}>
-                {props.children != null ? props.children : ''}
-            </div>
+        <div className={styles['statistics-block-wrapper'] + ' ' + altStyle}>
+            {
+                props.heading != null ?  (
+                    <h2 className={'h2 ' + styles['statistics-block-heading']}>{props.heading}</h2>
+                ) : ''
+            }
+            {
+                props.children != null ?  (
+                    <div className={styles['statistics-block-description']}>
+                        {props.children}
+                    </div>
+                ) : ''
+            }
             <div className={styles['statistics-block-data']}>
                 {props.statsData.length != 0 || props.statsData != null
                     ? props.statsData.map((stat, i) => (
