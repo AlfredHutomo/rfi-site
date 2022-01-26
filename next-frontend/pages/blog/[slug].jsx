fc036d2e6cd2c02cc10234ad90cff3bc9bde20a4
Alfred Hutomo <alfredhutomo@gmail.com>
2022-01-26 22:07:09 +0700

    header footer backend added

--- next-frontend/pages/blog/[slug].jsx
+++ next-frontend/pages/blog/[slug].jsx
@@ -7,48 +7,6 @@ const BlogArticle = ({ blogData, layoutData }) => {
     return (
         <PageWrapper layoutData={layoutData}>
             <BlogLayout data={blogData}></BlogLayout>
-
-            {/* <PostList
-                isMobileSlider
-                data={[
-                    {
-                        //image: '',
-                        title: 'Announcement: Santiago Ortiz appointed to coaching role at Green Gulley',
-                        date: '15 Sep',
-                        author: 'RFI Management Team',
-                        description:
-                            'We are excited to provide you with news of the appointment of Santiago Ortiz – Santi – to a coaching role at Green Gully in the NPL for season 2022.',
-                        link: 'http://www.google.com.au',
-                    },
-                    {
-                        //image: '',
-                        title: 'What are the benefits of taking part in football classes for kids?',
-                        date: '15 Sep',
-                        author: 'RFI Management Team',
-                        description:
-                            'We are excited to provide you with news of the appointment of Santiago Ortiz – Santi – to a coaching role at Green Gully in the NPL for season 2022.',
-                        link: '#',
-                    },
-                    {
-                        //image: '',
-                        title: 'The rise of ladies football',
-                        date: '15 Sep',
-                        author: 'RFI Management Team',
-                        description:
-                            'We are excited to provide you with news of the appointment of Santiago Ortiz – Santi – to a coaching role at Green Gully in the NPL for season 2022.',
-                        link: '#',
-                    },
-                    {
-                        //image: '',
-                        title: 'Why use different sized footballs for different age groups?',
-                        date: '15 Sep',
-                        author: 'RFI Management Team',
-                        description:
-                            'We are excited to provide you with news of the appointment of Santiago Ortiz – Santi – to a coaching role at Green Gully in the NPL for season 2022.',
-                        link: '#',
-                    },
-                ]}
-            ></PostList> */}
         </PageWrapper>
     );
 };
