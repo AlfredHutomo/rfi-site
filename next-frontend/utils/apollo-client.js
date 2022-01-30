6d8d39ffe47dca8022ef85f33dc6723ce8a1dbfa
Alfred Hutomo <alfredhutomo@gmail.com>
2022-01-30 16:39:52 +0700

    WIP Next.js deployment

--- next-frontend/utils/apollo-client.js
+++ next-frontend/utils/apollo-client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 export const getApolloClient = () => {
     return new ApolloClient({
         uri:
-            process.env.NEXT_PUBLIC_STRAPI_API_URL ||
+            `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/graphql` ||
             'http://localhost:8080/graphql',
         cache: new InMemoryCache(),
     });
