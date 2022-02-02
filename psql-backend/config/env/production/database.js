e2e326b0bb6c5c2acbeb2ca71dd65f05825d5d21
Alfred Hutomo <alfredhutomo@gmail.com>
2022-02-02 19:44:11 +0700

    fixed production database config

--- psql-backend/config/env/production/database.js
+++ psql-backend/config/env/production/database.js
@@ -1,23 +1,39 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = () => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
-        },
+console.log(config);
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
       },
-      options: {},
     },
   },
+  // TOTALLY OUTDATED DOCUMENTATION
+  // defaultConnection: "default",
+  // connections: {
+  //   default: {
+  //     connector: "bookshelf",
+  //     settings: {
+  //       client: "postgres",
+  //       host: config.host,
+  //       port: config.port,
+  //       database: config.database,
+  //       username: config.user,
+  //       password: config.password,
+  //       ssl: {
+  //         rejectUnauthorized: false,
+  //       },
+  //     },
+  //     options: {},
+  //   },
+  // },
 });
