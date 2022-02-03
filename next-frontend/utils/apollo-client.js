6fb08e33808400551a215e025733fe83cb43e63b
Alfred Hutomo <alfredhutomo@gmail.com>
2022-02-03 15:51:04 +0700

    apolloClient reverted

--- next-frontend/utils/apollo-client.js
+++ next-frontend/utils/apollo-client.js
@@ -1,11 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-export const getApolloClient = ({ clientSide = false }) => {
+export const getApolloClient = () => {
     return new ApolloClient({
         uri:
             process.env.NEXT_PUBLIC_STRAPI_GQL ||
             'http://localhost:8080/graphql',
         cache: new InMemoryCache(),
-        ssrMode: clientSide,
     });
 };
