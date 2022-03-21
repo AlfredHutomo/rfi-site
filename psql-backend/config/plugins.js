c457d5655fe8a0c2504db71ff3bbdf4973a8a298
AlfredRelab <alfred@relab.com.au>
2022-03-21 14:35:31 +0700

    moved backend plugin endpoints to env variables

--- psql-backend/config/plugins.js
+++ psql-backend/config/plugins.js
@@ -5,9 +5,9 @@ module.exports = ({ env }) => ({
       providerOptions: {
         accessKeyId: env("AWS_ACCESS_KEY_ID"),
         secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: "ap-southeast-2",
+        region: env("AWS_S3_REGION"),
         params: {
-          Bucket: "rfi-site",
+          Bucket: env("AWS_S3_BUCKET"),
         },
       },
     },
@@ -15,28 +15,10 @@ module.exports = ({ env }) => ({
   "website-builder": {
     enabled: true,
     config: {
-    //url: "https://api.vercel.com/v1/integrations/deploy/prj_tTSSXbwDKQHVK8YPVVwhjBqIiq1R/96cl12i8hS",
-      url: "https://api.vercel.com/v1/integrations/deploy/prj_tTSSXbwDKQHVK8YPVVwhjBqIiq1R/az0MhzdAQW",
+      url: env("VERCEL_BUILD_HOOK"),
       trigger: {
         type: "manual",
       },
-      // trigger: {
-      //   type: "event",
-      //   events: [
-      //     {
-      //       model: "page",
-      //       types: ["create", "update", "delete"],
-      //     },
-      //     {
-      //       model: "program",
-      //       types: ["create", "update", "delete"],
-      //     },
-      //     {
-      //       model: "blog",
-      //       types: ["create", "update", "delete"],
-      //     },
-      //   ],
-      // },
     },
   },
 });
